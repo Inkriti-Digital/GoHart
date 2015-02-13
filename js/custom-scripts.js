428244d23bcb0f70d0679dcb2b3e5e05a1f9a6ff
jkipker <jkipker@gmail.com>
2015-02-12 22:51:39 -0800

    news navigation functionality

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    var newsWidth;
+    var animateWidth = 0;
 
 
 	resizeElements();
@@ -9,6 +11,8 @@ $(document).ready(function(){
     });
 
     function resizeElements(){
+        newsWidth = $('.news-blocks').width();
+        // console.log('news width = ' + newsWidth);
     	var landingWidth = $('.landing').width();
     	var landingHeight = Math.floor(landingWidth*0.58214);
     	$('.landing').css('height', landingHeight);
@@ -28,16 +32,63 @@ $(document).ready(function(){
     }
 
 
+    //HOVER CONTROLS
      $(".metro-block").hover(function() {
         console.log('hit in');
-         $(this).closest(".metro-block").find(".hover-box .metro-hover").fadeIn();
-         $(this).closest(".metro-block").find("img").css('opacity', '0.5');          
-        }, function() {
+         $(this).closest(".metro-block").find(".hover-box .metro-hover").fadeIn();        
+         $(this).closest(".metro-block").find("img").animate({
+            opacity: 0.5 }, 500, function() { /*complete*/});
+         }, function() {
             console.log('hit out');
-         $(this).closest(".metro-block").find(".hover-box .metro-hover").fadeOut();   
-         $(this).closest(".metro-block").find("img").css('opacity', '1');
+         $(this).closest(".metro-block").find(".hover-box .metro-hover").fadeOut();            
+         $(this).closest(".metro-block").find("img").animate({
+            opacity: 1          
+          }, 500, function() { /* Animation complete.*/ });
         }
-    );    
+    );
+
+    //metrobox nav links
+    $(".metro-box a").hover(function() {   
+            $(".arrow", this).fadeIn(50);
+    }, function() { 
+            $(".arrow", this).fadeOut(50);
+        }
+    );
+
+    //news controls
+    $(".news-blocks .prev-arrow").click(function(event) {
+        animateWidth+=newsWidth;
+        console.log('animate' + animateWidth);
+        setTimeout(function() {
+            console.log('animate' + animateWidth);
+                $("#newsRow1").animate({
+                    left: animateWidth
+                }, 2500);
+            }, 300);
+        setTimeout(function() {
+            $("#newsRow2").animate({
+                left: animateWidth
+            }, 2500);
+        }, 100);
+    });
+
+     $(".news-blocks .next-arrow").click(function(event) {
+        animateWidth-=newsWidth;
+        console.log('animate' + animateWidth);
+        setTimeout(function() {
+            console.log('animate' + animateWidth);
+                $("#newsRow1").animate({
+                    left: animateWidth
+                }, 2250);
+            }, 300);
+        setTimeout(function() {
+            $("#newsRow2").animate({
+                left: animateWidth
+            }, 2250);
+        }, 100);
+    });
+
+
 
    
 
