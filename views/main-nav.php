d6c3e31e6a40177858126edee7d46afa3e093d90
jkipker <jkipker@gmail.com>
2015-02-24 13:22:27 -0800

    main-nav added

--- views/main-nav.php
+++ views/main-nav.php
@@ -1,3 +1,28 @@
 <div class="main-nav">
-	<img src="img/fpo-mainnav.png"/>
+	<!-- <img src="img/fpo-mainnav.png"/> -->
+
+	<div class="primary">
+		<ul>
+			<li><a href="#">MAPS &amp; SCHEDULES</a></li>
+			<li><a href="#">FARES</a></li>
+			<li><a href="#">SERVICES</a></li>
+			<li><a href="#">TRAVEL INFO</a></li>
+			<li><a href="#">CUSTOMER SERVICE</a></li>
+			<li><a href="#">TRANSPARENCY</a></li>
+			<li><a href="#">ABOUT</a></li>
+		</ul>
+
+		<div class="search-nav">
+			<a href="#">SITEMAP</a> &bull; 
+			<a href="#">GOOGLE TALK</a> &bull; 
+			<a href="#">ONLINE CHAT</a>
+		</div>
+
+		<div class="search-bar">
+			<form action="#">
+				<input id="start" type="text" name="start" value="SEARCH">
+				<div class="search-btn"><img src="img/search-ico.png"/></div>
+			</form>
+		</div>
+	</div>
 </div>
