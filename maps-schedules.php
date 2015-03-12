d1fc68c1211bc7d36457fcc4cef6045d03538d40
jkipker <jkipker@gmail.com>
2015-03-11 22:17:19 -0700

    maps-schedules done

---
+++ maps-schedules.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>GoHart</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/styles.min.css">
+	</head>
+	<body>
+
+		<!-- Content -->
+
+		<div class="container maps-scheduels">
+			<?php include ("views/main-nav.php"); ?>
+			
+			<section class="header">
+				<div class="logo"><img src="img/hart-logo.png"/></div>
+				<div class="menu-ico"><img src="img/menu-ico.png"/></div>
+
+				<div class="headline">
+					<h1>MAPS &amp;</h1>
+					<h1>SCHEDULES</h1>
+				</div>
+
+			</section>
+			<section class="page-content planner">
+
+				<div class="content-block content-body">
+					<div class="bottom-rule">
+						<div class="cta"><span>SYSTEM MAP</span><hr/></div>
+					</div>
+					<img src="img/maps-sched/map-full.jpg"/>
+					<div class="map-descript">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pellentesque consequat laoreet. 
+							Lorem ipsum dolor sit amet, consectetur adipiscing  
+						</p>
+					</div>
+								
+					<div class="bottom-rule">
+						<div class="cta"><span>SCHEDULES</span><hr/></div>
+					</div>
+
+					<div class="schedule-nav">
+						<a href="#">BUS</a>
+						<a href="#">METRORAPID</a>
+						<a href="#">VAN</a>
+						<a href="#">STREETCAR SYSTEM</a>
+						<a href="#">ALTERNATIVE TRANSPORTATION</a>
+					</div>
+
+					<div class="half-column">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In Lorem ipsum dolor sit amet, consectetur adipiscing elit. In Lorem ipsum dolor sit amet, consectetur adipiscing elit. In Lorem ipsum dolor sit amet, consectetur adipiscing elit. In Lorem ipsum dolor sit amet, consectetur adipiscing elit. In </p>
+					</div>
+
+					<div class="half-column">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In Lorem ipsum dolor sit amet, consectetur adipiscing elit. In Lorem ipsum dolor sit amet, consectetur adipiscing elit. In Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						<div class="download-pdf"><a href="#"><img src="img/download-pdf.png"/><span>Download PDF</span></a></div>
+						</p>
+					</div>
+
+					<div class="schedule-wrap">
+						<h2>METRORAPID – WEEKDAY – SOUTHBOUND</h2>
+						<h2>Hidden River Regional Park-n-Ride/UATC to Downtown Tampa</h2>
+
+						<div class="schedule-grid">
+							<div class="row title-row">
+								<div class="cell"></div>
+								<div class="cell">Hidden River Regional<br/>Park-n-Ride</div>
+								<div class="cell">University Area Transit Center</div>
+								<div class="cell">Nebraska Ave. @ Busch Blvd.</div>
+								<div class="cell">Nebraska Ave. @ Hillsborough Ave.</div>
+								<div class="cell">Marion Transit Center</div>
+								<div class="cell">Nebraska Ave. @ M.L. King Jr. Blvd.</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="letter-row">
+								<div class="cell"></div>
+								<div class="cell">E</div>
+								<div class="cell">D</div>
+								<div class="cell">C</div>
+								<div class="cell">B</div>
+								<div class="cell">A</div>
+								<div class="cell">MTC</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="time-row">
+								<div class="cell ampm"></div>
+								<div class="cell">LEAVE</div>
+								<div class="cell"></div>
+								<div class="cell"></div>
+								<div class="cell"></div>
+								<div class="cell"></div>
+								<div class="cell">ARRIVE</div>
+								<div class="clear-fix"></div>
+							</div>
+
+							<!--TO BE POPULATED FROM DATABASE-->
+							<div class="time-row">
+								<div class="cell ampm">A.M.</div>
+								<div class="cell">5:20</div>
+								<div class="cell">5:38</div>
+								<div class="cell">5:47</div>
+								<div class="cell">5:50</div>
+								<div class="cell">6:02</div>
+								<div class="cell">MTC</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="time-row">
+								<div class="cell ampm">A.M.</div>
+								<div class="cell">5:20</div>
+								<div class="cell">5:38</div>
+								<div class="cell">5:47</div>
+								<div class="cell">5:50</div>
+								<div class="cell">6:02</div>
+								<div class="cell">MTC</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="time-row">
+								<div class="cell ampm">A.M.</div>
+								<div class="cell">5:20</div>
+								<div class="cell">5:38</div>
+								<div class="cell">5:47</div>
+								<div class="cell">5:50</div>
+								<div class="cell">6:02</div>
+								<div class="cell">MTC</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="time-row">
+								<div class="cell ampm">A.M.</div>
+								<div class="cell">5:20</div>
+								<div class="cell">5:38</div>
+								<div class="cell">5:47</div>
+								<div class="cell">5:50</div>
+								<div class="cell">6:02</div>
+								<div class="cell">MTC</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="time-row">
+								<div class="cell ampm">A.M./P.M.</div>
+								<div class="cell">5:20</div>
+								<div class="cell">5:38</div>
+								<div class="cell">5:47</div>
+								<div class="cell">5:50</div>
+								<div class="cell">6:02</div>
+								<div class="cell">MTC</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="row title-row">
+								<div class="cell"></div>
+								<div class="cell">Hidden River Regional<br/>Park-n-Ride</div>
+								<div class="cell">University Area Transit Center</div>
+								<div class="cell">Nebraska Ave. @ Busch Blvd.</div>
+								<div class="cell">Nebraska Ave. @ Hillsborough Ave.</div>
+								<div class="cell">Marion Transit Center</div>
+								<div class="cell">Nebraska Ave. @ M.L. King Jr. Blvd.</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="letter-row">
+								<div class="cell"></div>
+								<div class="cell">E</div>
+								<div class="cell">D</div>
+								<div class="cell">C</div>
+								<div class="cell">B</div>
+								<div class="cell">A</div>
+								<div class="cell">MTC</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="time-row">
+								<div class="cell ampm"></div>
+								<div class="cell">LEAVE</div>
+								<div class="cell"></div>
+								<div class="cell"></div>
+								<div class="cell"></div>
+								<div class="cell"></div>
+								<div class="cell">ARRIVE</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="time-row">
+								<div class="cell ampm">A.M./P.M.</div>
+								<div class="cell">5:20</div>
+								<div class="cell">5:38</div>
+								<div class="cell">5:47</div>
+								<div class="cell">5:50</div>
+								<div class="cell">6:02</div>
+								<div class="cell">MTC</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="time-row">
+								<div class="cell ampm">A.M./P.M.</div>
+								<div class="cell">5:20</div>
+								<div class="cell">5:38</div>
+								<div class="cell">5:47</div>
+								<div class="cell">5:50</div>
+								<div class="cell">6:02</div>
+								<div class="cell">MTC</div>
+								<div class="clear-fix"></div>
+							</div>
+							<div class="time-row">
+								<div class="cell ampm">A.M./P.M.</div>
+								<div class="cell">5:20</div>
+								<div class="cell">5:38</div>
+								<div class="cell">5:47</div>
+								<div class="cell">5:50</div>
+								<div class="cell">6:02</div>
+								<div class="cell">MTC</div>
+								<div class="clear-fix"></div>
+							</div>
+
+
+						</div>
+					</div>
+
+
+					<div class="bottom-rule footer-rule">
+						<div class="cta center-cta"><img src="img/trips/lower-cta.png"/><hr/></div>
+					</div>
+
+
+
+					<div class="center footer-ad">
+							<img src="img/service/fpo-ad.jpg"/>
+						</div>
+					</div>
+				</div>				
+			</section>
+		</div>
+
+		<!-- Load Scripts -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<!--script src="js/vendors.js"></script-->
+		<script src="js/custom-scripts.js"></script>
+
+	</body>
+</html>
