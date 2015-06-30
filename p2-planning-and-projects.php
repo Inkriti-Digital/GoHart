8938eba50bd3f547ed0347ff8818c90ba2c84f3b
jkipker <jkipker@gmail.com>
2015-06-30 12:12:27 -0700

    project planning page complete

--- p2-planning-and-projects.php
+++ p2-planning-and-projects.php
@@ -141,215 +141,157 @@
 						</div>
 					</div>
 					<div class="bottom-rule">
-						<div class="cta"><span>RULES</span><hr/></div>
+						<div class="cta"><span>HART METRORAPID</span><hr/></div>
 					</div>
 					<div class="content-body">
-						<div class="fullColumnWidth">
+						<div class="half-column left">
 							<p>
-								The following are necessary to ensure a safe and efficient transportation system. Failure to comply with these rules, regulations, procedures, or guidelines may result in the denial of service and/or arrest.
+								With limited stops, advanced technology, new modern station stops 
 							</p>
 						</div>
-						
-						<div class="fullColumnWidth">	
-							<h2>ONBOARD BUSES, TROLLEYS AND STREETCARS</h2>
+						<div class="half-column right">
+							HART METRORAPID
 						</div>
+						<div class="clear-fix"></div>
 						
-						<div class="fullColumnWidth brick-list">
-							<div class="half-column brick-column left">
-								<ul class="no-margin">
-									<li>No weapons</li>
-									<li>No alcoholic beverages, controlled substances, or illegal use of paint or glue</li>
-									<li>No smoking, eating, or drinking of any beverage</li>
-									<li>No running in the aisles or playing loud music</li>
-									<li>Speak softly when using a cell phone</li>
-									<li>Keep entrances, exits and aisles clear</li>
-									<li>No rude, abusive, or vulgar language</li>
-									<li>No abusive or threatening behavior towards others</li>
-									<li>No loitering around bus stops</li>
-								</ul>
-							</div>
-							<div class="half-column brick-column brick-column-last right">
-								<ul class="no-margin">
-									<li>Do not write, paint, or destroy and bus, bus property, or bus stop</li>
-									<li>No boarding bus wearing roller blades or roller skates</li>
-									<li>Shoes and shirts are required</li>
-									<li>Stay in your seats until the bus comes to a halt</li>
-									<li>Keep your hands and head inside the bus</li>
-									<li>Stay clear of the doors until they are opened</li>
-									<li>Do no cross the street in front of a bus after exiting the vehicle</li>
-									<li>Do not open the windows</li>
-									<li>Report suspicious packages or behavior to the operator</li>
-								</ul>
-							</div>
-							<div class="clear-fix"></div>
-						</div>
-					
-					</div>	
+					</div>
 					<div class="bottom-rule">
-						<div class="cta"><span>BIKES ON BUSES</span><hr/></div>
+						<div class="cta"><span>BUS SHELTER AND INFRASTRUCTURE IMPROVEMENTS</span><hr/></div>
 					</div>
-					<div class="content-body">
-					
+					<div class="content-body">					
 						<div class="fullColumnWidth">
 							<p>
-								HART makes it easy to ride the bus with our Bikes on Buses program. By combining two modes of transportation, you can greatly expand your range of travel.
+								HART is committed to providing quality service through realignments and upgrades of bus stops and bus shelters throughout the county. For the most part, these improvements are carried out on a route by route basis. For these projects, HART works in collaboration with local jurisdictions, developers and private property owners to engage in cost sharing opportunities or establish right-of-way easements for placement of transit infrastructure. Where other agencies implement roadway improvements or sidewalk installations, HART works with these entities to incorporate shelter/landing pads and bus bays where appropriate.
 							</p>
-						</div>
-						
-						<div class="fullColumnWidth brick-list">
-							<div class="half-column brick-column left">
-								<h2>FOLDED BIKES</h2>
-								<div>
-									Folded bikes <a href="#" class="bold-blue">are permitted on all HART buses and HARTFlex vans</a>. Just a few courtesies to keep in mind:
+
+
+							<div class="comments planning-project-form">
+								<div class="comment-form">
+									<form action="#" id="comment-form1">
+										<div class="form-half form-left">
+											<input type="text" name="name" required="required" placeholder="*NAME">
+											<input type="text" name="address" placeholder="ADDRESS">
+											<input type="text" name="city" placeholder="CITY">
+											<input type="text" name="state" placeholder="STATE">
+											<input type="text" name="zip" placeholder="ZIP">
+											<input type="text" name="phone" required="required" placeholder="*PHONE">
+											<input type="text" name="fax" placeholder="FAX">
+											<input type="text" name="email" 
+											required="required" placeholder="*EMAIL">
+											<input type="text" name="bus-stop-id" required="required" placeholder="*BUS STOP ID NUMBER">
+											<input type="text" name="bus-routes-served" required="required" placeholder="*BUS ROUTES SERVED">
+										</div>
+										<div class="form-half">
+											<input type="text" name="street" 
+											required="required" placeholder="*STREET">
+											<input type="text" name="side-of-street" required="required" placeholder="*WHICH SIDE OF STREET?">
+											<input type="text" name="side-of-street" required="required" placeholder="*NEAREST CROSS STREET">
+											<input type="text" name="side-of-street" required="required" placeholder="*WHICH SIDE OF CROSS STREET?">
+											<h2>WOULD YOU LIKE RESULTS OF EVALUATION?</h2>
+											<input type="radio" name="yes" value="male">Yes
+											<input type="radio" name="no" value="female">No
+											<br/><br/>
+											<textarea name="comment" required="required" placeholder="*COMMENTS"></textarea>
+
+											<div class="submit-btn"><input type="submit" value="SUBMIT"></div>
+										</div>
+										<div class="clear-fix"></div>						
+									</form>
 								</div>
-								<ul class="no-margin">
-									<li>Big bikes go on the rack - Maximum wheel size for folding bicycles is 20 inches</li>
-									<li>Be ready - Bicycles must be folded and bagged before boarding the bus or van</li>
-									<li>Folded bicycles go underneath the seat or in front of the customer</li>
-									<li>Motor-powered bicycles are not permitted inside HART vehicles or on bus/van bicycle racks</li>
+							</div>
+						</div>
+					</div>
+					<div class="bottom-rule">
+						<div class="cta"><span>PLANNING DOCUMENTS</span><hr/></div>
+					</div>
+					<div class="content-body">						
+						<div class="fullColumnWidth">							
+							<div class="half-column brick-column left">
+								
+								<ul>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
 								</ul>
 							</div>
 							<div class="half-column brick-column brick-column-last right">
-								<h2>BICYCLE RACKS</h2>
-								<div>
-									Bicycle racks are on all HART buses and HARTFlex vans. The following bicycle rack rules apply:
-								</div>
-								<ul class="no-margin">
-									<li>Each bus and van bicycle rack accommodates two bicycles</li>
-									<li>Only single-seat, two-wheel bicycles are permitted</li>
-									<li>If the rack is full, customers should be prepared to wait for the next bus or choose a safe location to lock up their bicycle</li>
+								
+								<ul>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
 								</ul>
-								<div>
-									You can also take your bike with you and transfer to a PSTA bus without a special permit. Go to <a href="#" class="bold-blue">www.psta.net</a> for more information.
-								</div>
+								
 							</div>
 							<div class="clear-fix"></div>
 						</div>
-						
-						
-						<div class="fullColumnWidth">
-							<div class="half-column brick-blue-link left">
-								<a href="#" class="brick-link">
-									<div class="icon">
-										<img src="img/schedule-img.png" alt="schedule"/>
-									</div>
-									<div class="brick-blue-link-text">
-										<div class="brick-blue-link-text1">
-											HOW TO BRING YOUR
-										</div>
-										<div class="brick-blue-link-text2">
-											BIKE ON BOARD
-										</div>
-									</div>
-									<div class="clear-fix"></div>
-								</a>
+					</div>
+					<div class="bottom-rule">
+						<div class="cta"><span>DATA DOWNLOADS</span><hr/></div>
+					</div>
+					<div class="content-body">
+						<div class="fullColumnWidth">							
+							<div class="half-column brick-column left">								
+								<ul>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+								</ul>
 							</div>
-							<div class="half-column brick-blue-link brick-blue-link-last right">
-								<a href="#" class="brick-link">
-									<div class="icon">
-										<img src="img/arrow.png" alt="nav arrow"/>
-									</div>
-									<div class="brick-blue-link-text">
-										<div class="brick-blue-link-text1">
-											TAKE YOUR BIKE ON A PSTA BUS
-										</div>
-										<div class="brick-blue-link-text2">
-											WWW.PSTA.NET
-										</div>
-									</div>
-									<div class="clear-fix"></div>
-								</a>
+							<div class="half-column brick-column brick-column-last right">
+								
+								<ul>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+									<li><a href="#">Lorem ipsum,</a> dolor sit amet consectetur adipiscing elit tristique</li>
+								</ul>
+								
 							</div>
 							<div class="clear-fix"></div>
 						</div>
-						
-						
+					</div>
+					<div class="bottom-rule">
+								<div class="cta"><span>PLANNING AND PROJECTS DASHBOARD</span><hr/></div>
+							</div>
+					<div class="content-body maps-scheduels">
 						<div class="fullColumnWidth">
-							<p>
-								You should attach an identification number to your bicycle — whether it's the manufacturer's serial number, a license plate or a sturdy decal — and keep a copy of this number with them. This will greatly facilitate the process of claiming lost bicycles. In addition, the Tampa Police Department encourages bicyclists to register their bicycles with its Bicycle Registration Theft Protection Program to assist in recovering stolen bikes.
-							</p>
+							<div class="schedule-nav projectplan-nav">					
+								<a href="#" onclick="#">RIDERSHIP</a>											
+								<a href="#" onclick="#">COST EFFICIENCY</a>											
+								<a href="#" onclick="#">SAFETY</a>											
+								<a href="#">ON-TIME PERFORMANCE</a>	<br/>										
+								<a href="#">COMPLAINTS PER 100,000 PASSENGERS</a>										
+								<a href="#">MILES BETWEEN FAILURES</a>										
+							</div>	
+
+							<div class="download-project-pdf">
+								<a href="#"><img src="img/dl-icon.png"/> Download PDF</div>
+							</div>
+							<div class="project-chart">
+								<img src="img/project-plan-chart.jpg" alt="Projects & Planning Chart"/>
+							</div>
 						</div>
-						
+
 					</div>					
 				</div>				
 			</section>
 			
-			<section class="mid-section">
-				<div class="mid-copy alt-transport-midcopy-1">
-					<h3>
-						Reduce your impact on the environment
-						<br/>
-						- pedal power is green power!
-					</h3>
-				</div>
-			</section>
+		
 			
 			
 			<section class="page-content" style="background: none;">
 				<div class="content-block">
-					<div class="bottom-rule">
-						<div class="cta"><span>TRAVEL TRAINING PROGRAM</span><hr/></div>
-					</div>
-					<div class="content-body">
-						<div class="fullColumnWidth">
-							<p>
-								With this FREE program, you will ride with a HART team member and receive step-by-step instructions on using the transit guide, reading a passenger schedule and riding the bus. You will also be provided with route schedules, a transit guide and informational brochures. This service is available in both English and Spanish.
-							</p>
-						</div>
-						
-						<div class="fullColumnWidth">
-							<div class="half-column brick-blue-link left">
-								<a href="#" class="brick-link">
-									<div class="icon">
-										<img src="img/phone-icon.png" alt="phone icon" />
-									</div>
-									<div class="brick-blue-link-text">
-										<div class="brick-blue-link-text1">
-											SCHEDULE FREE TRAINING SESSION
-										</div>
-										<div class="brick-blue-link-text2">
-											813-384-6307
-										</div>
-									</div>
-									<div class="clear-fix"></div>
-								</a>
-							</div>
-							<div class="half-column brick-blue-link brick-blue-link-last right">
-								<a href="#" class="brick-link">
-									<div class="icon">
-										<img src="img/travel-info/mail-icon.png" alt="mail icon" />
-									</div>
-									<div class="brick-blue-link-text">
-										<div class="brick-blue-link-text1">
-											REQUEST FREE TRAINING SESSION
-										</div>
-										<div class="brick-blue-link-text2">
-											<!--<a href="mailto:SHEPPARDM@GOHART.ORG" class="link-normal">SHEPPARDM@GOHART.ORG</a>-->
-											SHEPPARDM@GOHART.ORG
-										</div>
-									</div>
-									<div class="clear-fix"></div>
-								</a>
-							</div>
-							<div class="clear-fix"></div>
-						</div>
-						
-						<div class="fullColumnWidth">
-							<p>
-								Your first ride on HART during the training is free. Call our Travel Training Coordinator today at <a href="#" class="bold-blue">(813) 384-6307</a> to schedule your training session. You can also email your request to <a href="mailto:sheppardm@goHART.org" class="bold-blue">sheppardm@goHART.org</a>. Please have a specific destination in mind.
-							</p>
-							<p>
-								Disabled persons are also eligible, provided their disability does not require special assistance or specifically trained personnel.
-							</p>
-						</div>
-						
-						
+					<div class="content-body">						
 						<div class="bottom-rule footer-rule">
 							<div class="cta center-cta">
 								<div class="callout corp-trans-callout">
 									<div>
 										<span class="cta-more">WANT TO FIND OUT MORE? VISIT</span>
-										<span class="cta-page"><a href="travelinfo-hart-app-center.php">APP CENTER</a></span>
+										<span class="cta-page"><a href="travelinfo-hart-app-center.php">BUSINESS</a></span>
 									</div>
 								</div>
 								<hr/>
