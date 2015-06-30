791656103efa07bf17005f29adf2379ef3dfabfb
jkipker <jkipker@gmail.com>
2015-06-30 05:53:42 -0700

    project and planning all content added

---
+++ p2-planning-and-projects.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>GoHart</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/styles.min.css">
+		<link rel="stylesheet" href="css/styles2.css">
+		<link rel="stylesheet" href="css/phase2.css"
+		<!--[if lte IE 10]>
+			<link rel="stylesheet" href="css/ie9.css">
+		<![endif]-->
+	</head>
+	<body>
+
+		<!-- Content -->
+
+		<div class="container services">
+			<?php include ("views/main-nav.php"); ?>
+			
+			<section class="header aboutBG">
+				<div class="logo">
+					<a href="/Pages/gohart-home.aspx">
+						<img class="full-logo desktop" src="/Style%20Library/GoHart/Images/hart-logo.png" alt="GoHart Logo"/>
+						<img class="box-logo desktop" style="display: none;" src="/Style%20Library/GoHart/Images/hart-logo-box.png" alt="GoHart Graphic Logo"/>
+						<img class="mobile" src="/Style%20Library/GoHart/Images/responsive/mobile-logo.png" alt="GoHart Mobile Logo"/>
+					</a>
+				</div>
+				<div class="menu-ico"><img src="img/menu-ico.png" alt="Main Menu"/></div>
+
+				<div class="headline">
+					<h1>PLANNING</h1>
+					<br/>
+					<h1>AND PROJECTS</h1>
+				</div>
+
+			</section>
+			<section class="page-content">
+
+				<div class="content-block">
+
+					<p class="italic-headline"><i>Here you will find some of the projects HART is working on that could one day take you where you want to go.</i></p>
+					<div class="bottom-rule">
+						<div class="cta"><span>CAPITOL PROJECTS</span><hr/></div>
+					</div>
+					<div class="content-body">
+						<div class="fullColumnWidth">
+							<p>
+								HART has a number of projects around the county that are not only providing Hillsborough County residents with even more transportation options, but also creating local, vital jobs.
+							</p>
+
+							<h2>TRANSITION OF HART FLEET TO COMPRESSED NATURAL GAS <br/>
+							COMPRESSED NATURAL GAS TRANSITION
+							</h2>
+							<p>
+								HART is transitioning our fleet of buses and vans to Compressed Natural Gas (CNG), with plans to have the full fleet operating on CNG by 2026.
+							</p>
+
+							<h2>WHAT IS COMPRESSED NATURAL GAS?</h2>
+							<p>CNG is a fossil fuel comprised mostly of methane (around 90%), small amounts of ethane, propane and other gases. It is lighter than air and burns almost completely. CNG combustion results in leftover carbon dioxide and water, the same molecular compound we exhale when we breathe. Natural gas can be used in the form of compressed natural gas (CNG) or liquefied natural gas (LNG).</p>
+
+							<h2>WHY COMPRESSED NATURAL GAS?</h2>
+							<p><b>Reduction of CO<sup>2</sup>  Emissions</b><br/>
+								CNG vehicles produce the fewest emissions and contain significantly less pollutants than gasoline. Natural gas produces 30% to 40% less CO<sup>2</sup>, which is a greenhouse gas, and carbon monoxide (CO) is reduced by more than 90%.
+								<ul>
+									<li>
+										
+									</li>
+									<li>
+										One HART CNG van is estimated to reduce CO<sup>2</sup> emissions by 9 tons per year.  With 28 CNG vans in operation in 2014, HART will reduce these emissions by 252 tons per year.
+									</li>
+									<li>
+										One HART CNG bus will reduce CO<sup>2</sup>  by 25 tons per year. HART is scheduled to have 22 CNG buses in operation in 2015; thereby reducing emissions by 550 tons.
+									</li>
+								</ul>
+								Imagine how much CO<sup>2</sup>  emissions will be reduced by HART when the full fleet is converted to Compressed Natural Gas.
+							</p>
+							<p><b>American-made</b><br/>
+								HART is doing its part. More than 99% of the natural gas used in the United States comes from domestic or other American sources. The United States imports over one billion dollars of crude oil every day. Estimates show the United States has well over a 100 year supply of natural gas, significantly lessening dependence on foreign oil supplies. The CNG industry helps support national security, U.S. jobs and the US economy. </p>
+
+								<div class="tix-box">
+									<div class="tix-row">
+										<div class="tix-type">COMPRESSED NATURAL GAS (CNG)</div>
+										<div class="tix-fare">$1.95</div>
+									</div>
+									<div class="tix-row blue-row">
+										<div class="tix-type">DIESEL</div>
+										<div class="tix-fare">$3.00</div>
+									</div>
+									<div class="tix-row">
+										<div class="tix-type">GAS</div>
+										<div class="tix-fare">$3.60</div>
+									</div>
+														
+								<div class="disclaimer"><sup>*</sup> Prices are per gallon. CNG and Diesel are HART pricing.  Gasoline prices will vary depending upon market conditions. </div>
+							</div>
+
+							<p>
+								Each HART van will reduce annual fuel costs by an estimated $4,908. Each CNG HART bus is estimated to reduce annual fuel costs by $16,022.
+							</p>
+
+							<p>
+								<b>Safer</b><br/>
+								People cook and heat their homes with natural gas.  Would you do the same in your home with diesel fuel, or gasoline? CNG is lighter than air. Should a CNG leak occur, the gas will disperse rapidly into the atmosphere. Diesel or gasoline, which is heavier than air, can spill on the ground creating fire hazards and potential pollution to waterways.
+							</p>
+
+							<p>
+								Fuel storage tanks on CNG vehicles are thicker than on gasoline or diesel vehicles.  CNG is stored under high pressure in tub-shaped cylinders attached to the rear, top, or undercarriage of the vehicles, and meet rigorous federally required safety standards. Some of the safety tests include heat and pressure extremes, gunfire, collisions and fires. They are constructed of high-strength materials designed to withstand impact, puncture and, in the case of fire, its pressure release devices provide a controlled venting of the gas rather than letting pressure build up in the tanks.
+							</p>
+							<p>
+								<b>Maintenance and Performance</b><br/>
+								The real savings is on the maintenance side relative to the engines. A forty-foot transit bus, of which the life is scheduled to be around 12 years, generally has to have an engine rebuilt within five or six years. Since CNG burns cleanly, it is estimated that roughly 80% of the engines can go the full life cycle of the vehicle.
+							</p>
+
+						</div>
+						<div class="brick-list">
+						<div class="half-column brick-column left">
+								<ul class="no-margin">
+								<li>
+									CNG engine run much quieter which will reduce noise pollution on the roadways and for passengers on the vehicles
+								</li>
+								<li>
+									CNG has a higher Octane rating of 130, which increases fuel efficiency
+								</li>
+								<li>
+									Pipes and mufflers last longer since CNG does not react to the metal
+								</li>
+								<li>
+									Since CNG is already in a gas form, Natural Gas Vehicles have superior starting and drivability, even under severe hot and cold weather conditions
+								</li>
+						</div>
+						<div class="half-column brick-column brick-column-last right">
+								<ul class="no-margin">
+									<li>These vehicles experience less knocking and no vapor locking</li>
+									<li>CNG vehicles do not require the use of diesel particulate filters or Diesel  Exhaust Fluid injected into the exhaust stream to reduce Nitrogen Oxide (NOx), both of which saves on maintenance costs for each vehicle</li>
+								</ul>
+							</div>
+							<div class="clear-fix"></div>
+						</div>
+					</div>
+					<div class="bottom-rule">
+						<div class="cta"><span>RULES</span><hr/></div>
+					</div>
+					<div class="content-body">
+						<div class="fullColumnWidth">
+							<p>
+								The following are necessary to ensure a safe and efficient transportation system. Failure to comply with these rules, regulations, procedures, or guidelines may result in the denial of service and/or arrest.
+							</p>
+						</div>
+						
+						<div class="fullColumnWidth">	
+							<h2>ONBOARD BUSES, TROLLEYS AND STREETCARS</h2>
+						</div>
+						
+						<div class="fullColumnWidth brick-list">
+							<div class="half-column brick-column left">
+								<ul class="no-margin">
+									<li>No weapons</li>
+									<li>No alcoholic beverages, controlled substances, or illegal use of paint or glue</li>
+									<li>No smoking, eating, or drinking of any beverage</li>
+									<li>No running in the aisles or playing loud music</li>
+									<li>Speak softly when using a cell phone</li>
+									<li>Keep entrances, exits and aisles clear</li>
+									<li>No rude, abusive, or vulgar language</li>
+									<li>No abusive or threatening behavior towards others</li>
+									<li>No loitering around bus stops</li>
+								</ul>
+							</div>
+							<div class="half-column brick-column brick-column-last right">
+								<ul class="no-margin">
+									<li>Do not write, paint, or destroy and bus, bus property, or bus stop</li>
+									<li>No boarding bus wearing roller blades or roller skates</li>
+									<li>Shoes and shirts are required</li>
+									<li>Stay in your seats until the bus comes to a halt</li>
+									<li>Keep your hands and head inside the bus</li>
+									<li>Stay clear of the doors until they are opened</li>
+									<li>Do no cross the street in front of a bus after exiting the vehicle</li>
+									<li>Do not open the windows</li>
+									<li>Report suspicious packages or behavior to the operator</li>
+								</ul>
+							</div>
+							<div class="clear-fix"></div>
+						</div>
+					
+					</div>	
+					<div class="bottom-rule">
+						<div class="cta"><span>BIKES ON BUSES</span><hr/></div>
+					</div>
+					<div class="content-body">
+					
+						<div class="fullColumnWidth">
+							<p>
+								HART makes it easy to ride the bus with our Bikes on Buses program. By combining two modes of transportation, you can greatly expand your range of travel.
+							</p>
+						</div>
+						
+						<div class="fullColumnWidth brick-list">
+							<div class="half-column brick-column left">
+								<h2>FOLDED BIKES</h2>
+								<div>
+									Folded bikes <a href="#" class="bold-blue">are permitted on all HART buses and HARTFlex vans</a>. Just a few courtesies to keep in mind:
+								</div>
+								<ul class="no-margin">
+									<li>Big bikes go on the rack - Maximum wheel size for folding bicycles is 20 inches</li>
+									<li>Be ready - Bicycles must be folded and bagged before boarding the bus or van</li>
+									<li>Folded bicycles go underneath the seat or in front of the customer</li>
+									<li>Motor-powered bicycles are not permitted inside HART vehicles or on bus/van bicycle racks</li>
+								</ul>
+							</div>
+							<div class="half-column brick-column brick-column-last right">
+								<h2>BICYCLE RACKS</h2>
+								<div>
+									Bicycle racks are on all HART buses and HARTFlex vans. The following bicycle rack rules apply:
+								</div>
+								<ul class="no-margin">
+									<li>Each bus and van bicycle rack accommodates two bicycles</li>
+									<li>Only single-seat, two-wheel bicycles are permitted</li>
+									<li>If the rack is full, customers should be prepared to wait for the next bus or choose a safe location to lock up their bicycle</li>
+								</ul>
+								<div>
+									You can also take your bike with you and transfer to a PSTA bus without a special permit. Go to <a href="#" class="bold-blue">www.psta.net</a> for more information.
+								</div>
+							</div>
+							<div class="clear-fix"></div>
+						</div>
+						
+						
+						<div class="fullColumnWidth">
+							<div class="half-column brick-blue-link left">
+								<a href="#" class="brick-link">
+									<div class="icon">
+										<img src="img/schedule-img.png" alt="schedule"/>
+									</div>
+									<div class="brick-blue-link-text">
+										<div class="brick-blue-link-text1">
+											HOW TO BRING YOUR
+										</div>
+										<div class="brick-blue-link-text2">
+											BIKE ON BOARD
+										</div>
+									</div>
+									<div class="clear-fix"></div>
+								</a>
+							</div>
+							<div class="half-column brick-blue-link brick-blue-link-last right">
+								<a href="#" class="brick-link">
+									<div class="icon">
+										<img src="img/arrow.png" alt="nav arrow"/>
+									</div>
+									<div class="brick-blue-link-text">
+										<div class="brick-blue-link-text1">
+											TAKE YOUR BIKE ON A PSTA BUS
+										</div>
+										<div class="brick-blue-link-text2">
+											WWW.PSTA.NET
+										</div>
+									</div>
+									<div class="clear-fix"></div>
+								</a>
+							</div>
+							<div class="clear-fix"></div>
+						</div>
+						
+						
+						<div class="fullColumnWidth">
+							<p>
+								You should attach an identification number to your bicycle — whether it's the manufacturer's serial number, a license plate or a sturdy decal — and keep a copy of this number with them. This will greatly facilitate the process of claiming lost bicycles. In addition, the Tampa Police Department encourages bicyclists to register their bicycles with its Bicycle Registration Theft Protection Program to assist in recovering stolen bikes.
+							</p>
+						</div>
+						
+					</div>					
+				</div>				
+			</section>
+			
+			<section class="mid-section">
+				<div class="mid-copy alt-transport-midcopy-1">
+					<h3>
+						Reduce your impact on the environment
+						<br/>
+						- pedal power is green power!
+					</h3>
+				</div>
+			</section>
+			
+			
+			<section class="page-content" style="background: none;">
+				<div class="content-block">
+					<div class="bottom-rule">
+						<div class="cta"><span>TRAVEL TRAINING PROGRAM</span><hr/></div>
+					</div>
+					<div class="content-body">
+						<div class="fullColumnWidth">
+							<p>
+								With this FREE program, you will ride with a HART team member and receive step-by-step instructions on using the transit guide, reading a passenger schedule and riding the bus. You will also be provided with route schedules, a transit guide and informational brochures. This service is available in both English and Spanish.
+							</p>
+						</div>
+						
+						<div class="fullColumnWidth">
+							<div class="half-column brick-blue-link left">
+								<a href="#" class="brick-link">
+									<div class="icon">
+										<img src="img/phone-icon.png" alt="phone icon" />
+									</div>
+									<div class="brick-blue-link-text">
+										<div class="brick-blue-link-text1">
+											SCHEDULE FREE TRAINING SESSION
+										</div>
+										<div class="brick-blue-link-text2">
+											813-384-6307
+										</div>
+									</div>
+									<div class="clear-fix"></div>
+								</a>
+							</div>
+							<div class="half-column brick-blue-link brick-blue-link-last right">
+								<a href="#" class="brick-link">
+									<div class="icon">
+										<img src="img/travel-info/mail-icon.png" alt="mail icon" />
+									</div>
+									<div class="brick-blue-link-text">
+										<div class="brick-blue-link-text1">
+											REQUEST FREE TRAINING SESSION
+										</div>
+										<div class="brick-blue-link-text2">
+											<!--<a href="mailto:SHEPPARDM@GOHART.ORG" class="link-normal">SHEPPARDM@GOHART.ORG</a>-->
+											SHEPPARDM@GOHART.ORG
+										</div>
+									</div>
+									<div class="clear-fix"></div>
+								</a>
+							</div>
+							<div class="clear-fix"></div>
+						</div>
+						
+						<div class="fullColumnWidth">
+							<p>
+								Your first ride on HART during the training is free. Call our Travel Training Coordinator today at <a href="#" class="bold-blue">(813) 384-6307</a> to schedule your training session. You can also email your request to <a href="mailto:sheppardm@goHART.org" class="bold-blue">sheppardm@goHART.org</a>. Please have a specific destination in mind.
+							</p>
+							<p>
+								Disabled persons are also eligible, provided their disability does not require special assistance or specifically trained personnel.
+							</p>
+						</div>
+						
+						
+						<div class="bottom-rule footer-rule">
+							<div class="cta center-cta">
+								<div class="callout corp-trans-callout">
+									<div>
+										<span class="cta-more">WANT TO FIND OUT MORE? VISIT</span>
+										<span class="cta-page"><a href="travelinfo-hart-app-center.php">APP CENTER</a></span>
+									</div>
+								</div>
+								<hr/>
+							</div>
+						</div>
+						<div class="center footer-ad">
+								<img src="img/service/fpo-ad.jpg"/>
+								<div class="gototop">
+									<img src="img/gotop.png" alt="return to top" />
+								</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		</div>
+
+		<!-- Load Scripts -->
+		<?php include ("views/scripts-load.php"); ?>
+
+	</body>
+</html>
