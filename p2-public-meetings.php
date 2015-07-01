b50b1c6b6d592c07e63d9102da826becb9dd8437
jkipker <jkipker@gmail.com>
2015-07-01 05:12:16 -0700

    public meetings layout complete

---
+++ p2-public-meetings.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>GoHart</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/styles.min.css">
+		<link rel="stylesheet" href="css/styles2.css">
+		<link rel="stylesheet" href="css/phase2.css"
+		<!--[if lte IE 10]>
+			<link rel="stylesheet" href="css/ie9.css">
+		<![endif]-->
+	</head>
+	<body>
+
+		<!-- Content -->
+
+		<div class="container services">
+			<?php include ("views/main-nav.php"); ?>
+			
+			<section class="header aboutBG">
+				<div class="logo">
+					<a href="/Pages/gohart-home.aspx">
+						<img class="full-logo desktop" src="/Style%20Library/GoHart/Images/hart-logo.png" alt="GoHart Logo"/>
+						<img class="box-logo desktop" style="display: none;" src="/Style%20Library/GoHart/Images/hart-logo-box.png" alt="GoHart Graphic Logo"/>
+						<img class="mobile" src="/Style%20Library/GoHart/Images/responsive/mobile-logo.png" alt="GoHart Mobile Logo"/>
+					</a>
+				</div>
+				<div class="menu-ico"><img src="img/menu-ico.png" alt="Main Menu"/></div>
+
+				<div class="headline">
+					<h1>PUBLIC</h1>
+					<br/>
+					<h1>MEETINGS</h1>
+				</div>
+
+			</section>
+			<section class="page-content">
+
+				<div class="content-block">
+					<div class="page-nav">
+						<a href="#" class="nav-btn"><span>TEAM</span></a>
+						<a href="#" class="nav-btn"><span>BOARD MEMBER LOGIN</span></a>						
+					</div>
+					
+					<div class="bottom-rule">
+						<div class="cta"><span>CAPITOL PROJECTS</span><hr/></div>
+					</div>
+					<div class="content-body">
+						<div class="fullColumnWidth">
+							<div class="info pub-meeting-info">
+								<p>
+									A copy of the agenda may be obtained by writing to HART, 1201 East 7th Avenue, Tampa, Florida 33605; or by contacting HART Clerk of the Board at <b>(813) 384-6552</b>. <br/><br/> Pursuant to provisions of the American with Disabilities Act, any person requiring special accommodations to participate in this meeting is asked to contact HART Clerk of the Board at <b>(813) 384-6552</b>  at least seven days before the meeting. <br/><br/> If you are hearing or speech impaired, please call the TDD <b>(813) 626-9158.</b> Section 286.0105, Florida Statutes, provides that if a person decides to appeal any decision made by a board, agency, or commission with respect to any matter considered at a meeting or hearing, he or she need a record of proceedings, and that, for such purpose, he or she may need to ensure that a verbatim record of the proceedings is made, which record include the testimony and evidence upon which the appeal is to be based.
+								</p>
+							</div>
+							<div class="info-graphic">
+								<img src="img/innovation-ad.png"/>
+							</div>	
+
+							<div>
+								<div class="calandar-box">
+									<div class="calander-title">
+										<span>MONDAY, FEB. 16, 2015</span>
+									</div>
+									<div class="calander-info">
+										<h3>HART Finance, Governance, and Administration Committee Meeting</h3>
+										9:00 AM  <br/>
+										HART Administrative Office<br/>
+										1201 E. 7th Avenue, 3rd Floor<br/>
+										Tampa, Fl 33605<br/>
+										Florida Conference Room
+									</div>
+								</div>
+								<div class="calandar-box">
+									<div class="calander-title">
+										<span>MONDAY, FEB. 16, 2015</span>
+									</div>
+									<div class="calander-info">
+										<h3>HART Finance, Governance, and Administration Committee Meeting</h3>
+										9:00 AM  <br/>
+										HART Administrative Office<br/>
+										1201 E. 7th Avenue, 3rd Floor<br/>
+										Tampa, Fl 33605<br/>
+										Florida Conference Room
+									</div>
+								</div>
+								<div class="calandar-box">
+									<div class="calander-title">
+										<span>MONDAY, FEB. 16, 2015</span>
+									</div>
+									<div class="calander-info">
+										<h3>HART Finance, Governance, and Administration Committee Meeting</h3>
+										9:00 AM  <br/>
+										HART Administrative Office<br/>
+										1201 E. 7th Avenue, 3rd Floor<br/>
+										Tampa, Fl 33605<br/>
+										Florida Conference Room
+									</div>
+								</div>
+							</div>
+
+							<div>
+								<div class="calandar-box">
+									<div class="calander-title">
+										<span>MONDAY, FEB. 16, 2015</span>
+									</div>
+									<div class="calander-info">
+										<h3>HART Finance, Governance, and Administration Committee Meeting</h3>
+										9:00 AM  <br/>
+										HART Administrative Office<br/>
+										1201 E. 7th Avenue, 3rd Floor<br/>
+										Tampa, Fl 33605<br/>
+										Florida Conference Room
+									</div>
+								</div>
+								<div class="calandar-box">
+									<div class="calander-title">
+										<span>MONDAY, FEB. 16, 2015</span>
+									</div>
+									<div class="calander-info">
+										<h3>HART Finance, Governance, and Administration Committee Meeting</h3>
+										9:00 AM  <br/>
+										HART Administrative Office<br/>
+										1201 E. 7th Avenue, 3rd Floor<br/>
+										Tampa, Fl 33605<br/>
+										Florida Conference Room
+									</div>
+								</div>
+								<div class="calandar-box">
+									<div class="calander-title">
+										<span>MONDAY, FEB. 16, 2015</span>
+									</div>
+									<div class="calander-info">
+										<h3>HART Finance, Governance, and Administration Committee Meeting</h3>
+										9:00 AM  <br/>
+										HART Administrative Office<br/>
+										1201 E. 7th Avenue, 3rd Floor<br/>
+										Tampa, Fl 33605<br/>
+										Florida Conference Room
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+						
+					
+					
+					<div class="bottom-rule">
+						<div class="cta"><span>BOARD PACKETS</span><hr/></div>
+					</div>
+					<div class="content-body">					
+						<div class="fullColumnWidth">
+							<p>
+								Handouts including the agenda, reports and more are typically given to attendees at HART meetings. If you were not able to make it to the meeting, you can download the packet below in PDF format.
+							</p>							
+						</div>
+					</div>
+					<div class="bottom-rule">
+						<div class="cta"><span>PLANNING DOCUMENTS</span><hr/></div>
+					</div>
+					<div class="content-body">
+						<div class="fullColumnWidth pdf-doc-list">							
+							<div class="half-column brick-column left">								
+								<ul>
+									<li><a href="#"><img src="img/dl-icon.png"/>November 17, 2014</a>  Finance, Governance and Administration Committee Meeting – Packet</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>November 3, 2014</a>  Regular Board Meeting – Packet</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>October 20, 2014</a>  Finance, Governance and Administration Committee Meeting – Packet</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>October 8, 2014 </a>Ad Hoc Committee on Selection of Federal and State Legislative Consultant Services Meeting – Packet</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>October 2014</a> Monthly Information Report – Packet</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>September 22, 2014</a> Second and Final Public Hearing on Fiscal Year 2015 Mileage Rate and Annual Budget and Regular Board Meeting – Packet</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>September 8, 2014</a> Regular Board Meeting – Presentation – Operations Division Key Performance Indicators (KPIs)</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>September 8, 2014</a> Regular Board Meeting – Presentation – On-Board Survey Results</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>September 8, 2014</a> First Public Hearing on Tentative Fiscal Year 2015 Mileage Rate and Annual Budget – Presentation – Proposed FY2015 Operating and Capital Budget</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>September 8, 2014</a> First Public Hearing on Tentative Fiscal Year 2015 Mileage Rate and Annual Budget and Regular Board Meeting – Packet</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>August 18, 2014</a> Finance, Governance and Administration Committee Meeting – Packet</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>August 4, 2014</a> Regular Board Meeting – Presentation – South County Circulator </li>
+								</ul>
+							</div>
+							<div class="half-column brick-column brick-column-last right">
+								
+								<ul>
+									<li><a href="#"><img src="img/dl-icon.png"/>August 4, 2014</a>   Regular Board Meeting – Presentation – South County Circulator </li>
+									<li><a href="#"><img src="img/dl-icon.png"/>August 4, 2014</a> Regular Board Meeting – Presentation – Operations Division KPIs </li>
+									<li><a href="#"><img src="img/dl-icon.png"/>August 4, 2014</a> Regular Board Meeting – Presentation – Update on Security at MTC and Onboard of Vehicles </li>
+									<li><a href="#"><img src="img/dl-icon.png"/>August 4, 2014</a> Regular Board Meeting – Presentation – TDP Alternatives </li>
+									<li><a href="#"><img src="img/dl-icon.png"/>August 4, 2014</a> Regular Board Meeting – Packet</li>
+									<li><a href="#"><img src="img/dl-icon.png"/>July 21, 2014</a> Regular Board Meeting – Presentation – Certifying the Proposed Ad Valorem Tax Mileage Rate for the Fiscal Year 2015 Budget </li>
+									<li><a href="#"><img src="img/dl-icon.png"/>July 31, 2014</a> Ad Hoc Committee on Overseeing Legal Services Selection Process Meeting – Packet </li>
+									<li><a href="#"><img src="img/dl-icon.png"/>July 21, 2014</a> Regular Board Meeting – Presentation – Selection Process for Federal and State Legislative Consulting Services </li>
+									<li><a href="#"><img src="img/dl-icon.png"/>July 21, 2014</a> Regular Board Meeting – Presentation – Downtown Transit Assets and Opportunities Study </li>
+									<li><a href="#"><img src="img/dl-icon.png"/>July 21, 2014</a> Regular Board Meeting – Packet </li>
+									<li><a href="#"><img src="img/dl-icon.png"/>June 16, 2014</a> Major Projects and Legislative Committee Meeting – Packet </li>
+									<li><a href="#"><img src="img/dl-icon.png"/>June 9, 2014</a> Strategic Planning Workshop – Agenda </li>
+									<li><a href="#"><img src="img/dl-icon.png"/>June 2, 2014</a>  Regular Board Meeting – Packet</li>
+									
+								</ul>
+								
+							</div>
+							<div class="clear-fix"></div>
+						</div>
+					</div>
+					<div class="bottom-rule">
+								<div class="cta"><span>BOARD MEMBER LOGIN</span><hr/></div>
+							</div>
+					<div class="content-body maps-scheduels">
+						<div class="fullColumnWidth">
+							
+							<div class="employee-login">
+							<div class="employee-login-form">
+								<form action="#">
+									<div class="form-half form-left">
+										<input type="text" name="username" value="" placeholder="USERNAME" required="required">
+										<a href="#">Forgot username?</a>
+									</div>
+									<div class="form-half">
+										<input type="password" name="password" value="" placeholder="PASSWORD" required="required">
+										
+										<a href="#">Forgot password?</a>
+									</div>
+									<div class="clear-fix"></div>
+									<div class="submit-btn"><input type="submit" value="LOGIN"></div>
+								</form>
+							</div>
+						</div>
+							
+						</div>
+					</div>					
+				</div>				
+			</section>
+			
+		
+			
+			
+			<section class="page-content" style="background: none;">
+				<div class="content-block">
+					<div class="content-body">						
+						<div class="bottom-rule footer-rule">
+							<div class="cta center-cta">
+								<div class="callout corp-trans-callout">
+									<div>
+										<span class="cta-more">WANT TO FIND OUT MORE? VISIT</span>
+										<span class="cta-page"><a href="#">EMPLOYEE LOGIN</a></span>
+									</div>
+								</div>
+								<hr/>
+							</div>
+						</div>
+						<div class="center footer-ad">
+								<img src="img/service/fpo-ad.jpg"/>
+								<div class="gototop">
+									<img src="img/gotop.png" alt="return to top" />
+								</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		</div>
+
+		<!-- Load Scripts -->
+		<?php include ("views/scripts-load.php"); ?>
+
+	</body>
+</html>
