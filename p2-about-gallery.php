9e76fed7e98bc4196e1d08acf36c71084fb23fde
jkipker <jkipker@gmail.com>
2015-07-09 15:52:21 -0700

    gallery near complete

---
+++ p2-about-gallery.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>GoHart</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/styles.min.css">
+		<link rel="stylesheet" href="css/styles2.css">
+		<!--[if lte IE 10]>
+			<link rel="stylesheet" href="css/ie9.css">
+		<![endif]-->
+
+		<!-- gallery styles defaults-->
+		<link rel="stylesheet"  href="css/lightslider.css"/>
+		<style>
+    	ul{
+			list-style: none outside none;
+		    padding-left: 0;
+            margin: 0;
+		}
+        .demo .item{
+            margin-bottom: 60px;
+        }
+		.content-slider li{
+		    background-color: #ed3020;
+		    text-align: center;
+		    color: #FFF;
+		}
+		.content-slider h3 {
+		    margin: 0;
+		    padding: 70px 0;
+		}
+		.about-gallery{
+			width: 100%;
+			text-align: center;
+			margin: auto;
+		}
+
+		.about-gallery .item{
+			width: 90%;
+			margin: auto;
+		}
+
+		.lSGallery li{
+			/*width: 300px !important;*/
+		}
+
+		.lSAction a{
+			  margin-top: -38%;
+			  height: 72px;
+			  width: 41px;
+		}
+
+		.lSAction .lSPrev{
+			background: url('img/news-arrow-left.png');
+		}
+		.lSAction .lSNext{
+			background: url('img/news-arrow-right.png');
+		}
+
+
+    </style>
+	</head>
+	<body>
+
+		<!-- Content -->
+
+		<div class="container services">
+			<?php include ("views/main-nav.php"); ?>
+			
+			<section class="header aboutPagesBg">
+				<div class="logo">
+					<a href="/Pages/gohart-home.aspx">
+						<img class="full-logo desktop" src="/Style%20Library/GoHart/Images/hart-logo.png" alt="GoHart Logo"/>
+						<img class="box-logo desktop" style="display: none;" src="/Style%20Library/GoHart/Images/hart-logo-box.png" alt="GoHart Graphic Logo"/>
+						<img class="mobile" src="/Style%20Library/GoHart/Images/responsive/mobile-logo.png" alt="GoHart Mobile Logo"/>
+					</a>
+				</div>
+				<div class="menu-ico"><img src="img/menu-ico.png" alt="Main Menu"/></div>
+
+				<div class="headline">
+					<h1>ABOUT</h1>
+					<br/>
+					<h1>HART</h1>
+				</div>
+
+			</section>
+			<section class="page-content">
+				<div class="content-block">
+					<div class="about-gallery">
+			        <div class="item">            
+			            <div class="clearfix" style="max-width:100%;">
+			                <ul id="image-gallery" class="gallery list-unstyled cS-hidden">
+			                    <li data-thumb="img/tmp-slider/thumb/cS-1.jpg"> 
+			                        <img src="img/tmp-slider/cS-1.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-2.jpg"> 
+			                        <img src="img/tmp-slider/cS-2.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-3.jpg"> 
+			                        <img src="img/tmp-slider/cS-3.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-4.jpg"> 
+			                        <img src="img/tmp-slider/cS-4.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-5.jpg"> 
+			                        <img src="img/tmp-slider/cS-5.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-6.jpg"> 
+			                        <img src="img/tmp-slider/cS-6.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-7.jpg"> 
+			                        <img src="img/tmp-slider/cS-7.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-8.jpg"> 
+			                        <img src="img/tmp-slider/cS-8.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-9.jpg"> 
+			                        <img src="img/tmp-slider/cS-9.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-10.jpg"> 
+			                        <img src="img/tmp-slider/cS-10.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-11.jpg"> 
+			                        <img src="img/tmp-slider/cS-11.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-12.jpg"> 
+			                        <img src="img/tmp-slider/cS-12.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-13.jpg"> 
+			                        <img src="img/tmp-slider/cS-13.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-14.jpg"> 
+			                        <img src="img/tmp-slider/cS-14.jpg" />
+			                         </li>
+			                    <li data-thumb="img/tmp-slider/thumb/cS-15.jpg"> 
+			                        <img src="img/tmp-slider/cS-15.jpg" />
+			                         </li>
+				                </ul>
+				            </div>
+				        </div>
+				        <!--div class="item">
+				            <ul id="content-slider" class="content-slider">
+				                <li>
+				                    <h3>1</h3>
+				                </li>
+				                <li>
+				                    <h3>2</h3>
+				                </li>
+				                <li>
+				                    <h3>3</h3>
+				                </li>
+				                <li>
+				                    <h3>4</h3>
+				                </li>
+				                <li>
+				                    <h3>5</h3>
+				                </li>
+				                <li>
+				                    <h3>6</h3>
+				                </li>
+				            </ul>
+				        </div-->
+				    </div>
+					
+					<div class="bottom-rule">
+						<div class="cta"><span>DRIVEN TO SERVE</span><hr/></div>
+					</div>
+					<div class="content-body">
+						<div class="info">
+							<p class="main-info-lineheight">
+								The Hillsborough Area Regional Transit Authority (HART) was created in October of 1979 to plan, finance, acquire, construct, operate and maintain mass transit facilities and supply transportation assistance in Hillsborough County. Today, we are Driven to Serve You through convenient, affordable public transportation options tailored to contemporary lifestyles. More people than ever in Hillsborough County are discovering benefits of our sophisticated, yet accessible and <span class="nowrap">rider-friendly,</span> network of transit options. We look forward to helping more and more of our community ride HART.
+							</p>
+							<p class="visit-link">
+								<a href="#">CLICK HERE TO VIEW THE HART CHARTER</a>
+							</p>
+						</div>
+						<div class="info-graphic">
+							<img src="img/service/info-graphic.jpg"/>
+							<br/>
+							<span class="paid-ad-text">Paid Ad</span>
+						</div>
+						
+						<p>
+							HART provides the following services:
+						</p>
+						<div class="fullColumnWidth brick-list">
+							<div class="half-column brick-column left">
+								<ul class="no-margin">
+									<li><a href="#" class="bold-blue">MetroRapid</a></li>
+									<li>Local Fixed Route and <a href="#" class="bold-blue">Express Bus Service</a></li>
+									<li><a href="#" class="bold-blue">Streetcar</a></li>
+									<li>Rubber-wheeled <a href="#" class="bold-blue">In-Town Trolleys</a></li>
+									<li><a href="#" class="bold-blue">Vanpool</a> & Guaranteed Ride Home Service</li>
+									<li><a href="#" class="bold-blue">HARTFlex</a> service in Brandon, Northdale, South County, South Tampa and Town 'N Country</li>
+									<li><a href="#" class="bold-blue"></a></li>
+								</ul>
+							</div>
+							<div class="half-column brick-column brick-column-last right">
+								<ul class="no-margin">
+									<li>Wheelchair accessibility on all buses and vans</li>
+									<li>Bicycle accessibility on all buses and <a href="#" class="bold-blue">HARTFlex</a> vans</li>
+									<li><a href="#" class="bold-blue">Paratransit Service</a></li>
+									<li><a href="#" class="bold-blue">Travel Planning Assistance</a> & updated scheduled arrival times, bus stop locations and service updates for Smart Phones</li>
+									<li><a href="#" class="bold-blue">Travel Training</a></li>
+								</ul>
+							</div>
+							<div class="clear-fix"></div>
+						</div>
+						
+						<div class="bottom-rule footer-rule">
+						<div class="cta center-cta">
+							<div class="callout corp-trans-callout">
+								<div>
+									<span class="cta-more">WANT TO FIND OUT MORE? VISIT</span>
+									<span class="cta-page"><a href="about-hart-team.php">TEAM</a></span>
+								</div>
+							</div>
+							<hr/>
+						</div>
+					</div>
+						<div class="center footer-ad">
+								<img src="img/service/fpo-ad.jpg"/>
+								<div class="gototop">
+									<img src="img/gotop.png" />
+								</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+
+		<!-- Load Scripts -->
+		<?php include ("views/scripts-load.php"); ?>
+
+		<!--image gallery js plugin & init-->
+		<script src="js/lightslider.js"></script>
+
+		<script>
+	    	$(document).ready(function() {
+				$("#content-slider").lightSlider({
+	                loop:true,
+	                keyPress:true
+	            });
+	            $('#image-gallery').lightSlider({
+	                gallery:true,
+	                item:1,
+	                thumbItem:3,
+	                slideMargin: 0,
+	                speed:500,
+	                auto:false,
+	                loop:true,
+	                verticalHeight:500,
+        			vThumbWidth:280,
+	                onSliderLoad: function() {
+	                    $('#image-gallery').removeClass('cS-hidden');
+	                }  
+	            });
+			});
+    	</script>
+
+	</body>
+</html>
